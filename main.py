7fe126afa695a83e1b86360716cc731e4af5c4ae
James <valeness84@gmail.com>
2015-04-19 00:45:44 -0500

    Added some build output information to main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from os.path import isfile, join
 import re
 
 BEGIN_TOKEN = '<%='
-END_TOKEN = '<%='
+END_TOKEN = '%>'
+
+nl = "\n"
 
 conditions = {' ': '', '"': '', "<%=": '', '%>': ''}
 
@@ -17,9 +19,11 @@ def trim(include, conditions):
 	return text
 
 def getlines(file):
-	main = open('templates~/{0}'.format(file))
+	main = open('{0}/{1}'.format(inc_path, file))
 	lines = main.readlines()
 	main.close()
+
+	print("Got lines for {0}".format(file))
 
 	return lines
 
@@ -28,7 +32,8 @@ def parsehtml(lines):
 	for line in lines:
 		if BEGIN_TOKEN in line and END_TOKEN in line:
 			file = trim(line, conditions)
-			with open('templates~/includes/{0}'.format(file), 'r') as f:
+			print("Found Include File: {0}".format(file))
+			with open('{0}/includes/{1}'.format(inc_path, file), 'r') as f:
 				inc_files[line_c] = f.read()
 			lines[line_c] = inc_files[line_c]
 			#print(file)
@@ -36,6 +41,12 @@ def parsehtml(lines):
 
 inc_path = 'templates~'
 files = [f for f in listdir(inc_path) if isfile(join(inc_path, f))]
+
+print(nl)
+print("Include Path: {0}".format(inc_path))
+print("Parse Conditions: {0}".format(conditions))
+print("Files: {0}".format(files))
+print(nl)
 
 for file in files:
 	lines = getlines(file)
