64af75c6411265d92d2e913280d692ce5f447ea0
Your Name <you@example.com>
2015-04-21 10:09:49 +0900

    changed files to f

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def trim(include, conditions):
 	text = text.strip()
 	return text
 
-def getlines(file):
-	main = open('{0}/{1}'.format(inc_path, file))
+def getlines(f):
+	main = open('{0}/{1}'.format(inc_path, f))
 	lines = main.readlines()
 	main.close()
 
-	print("Got lines for {0}".format(file))
+	print("Got lines for {0}".format(f))
 
 	return lines
 
@@ -31,9 +31,9 @@ def parsehtml(lines):
 	line_c = 0
 	for line in lines:
 		if BEGIN_TOKEN in line and END_TOKEN in line:
-			file = trim(line, conditions)
-			print("Found Include File: {0}".format(file))
-			with open('{0}/includes/{1}'.format(inc_path, file), 'r') as f:
+			f = trim(line, conditions)
+			print("Found Include File: {0}".format(f))
+			with open('{0}/includes/{1}'.format(inc_path, f), 'r') as f:
 				inc_files[line_c] = f.read()
 			lines[line_c] = inc_files[line_c]
 			#print(file)
@@ -48,9 +48,9 @@ print("Parse Conditions: {0}".format(conditions))
 print("Files: {0}".format(files))
 print(nl)
 
-for file in files:
-	lines = getlines(file)
+for f in files:
+	lines = getlines(f)
 	parsehtml(lines)
 
-	with open(file, 'w') as f:
+	with open(f, 'w') as f:
 		f.writelines(lines)
