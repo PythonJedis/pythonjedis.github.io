80b792d0158ba1a89b73d2be830179ca9a800f20
park <pash080@gmail.com>
2015-04-26 13:02:41 +0900

    added a locations category in index.html, main.py is python3 frinedly, added a mapPage to show a google map of our locations

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ conditions = {' ': '', '"': '', "<%=": '', '%>': ''}
 inc_files = {}
 
 def trim(include, conditions):
-	rep = dict((re.escape(k), v) for k, v in conditions.iteritems())
+	rep = dict((re.escape(k), v) for k, v in conditions.items())
 	pattern = re.compile("|".join(rep.keys()))
 	text = pattern.sub(lambda m: rep[re.escape(m.group(0))], include)
 	text = text.strip()
